47c80d31a7918e2186dee4e0b58f46d4b162ce6c
Hakim Tokhi <hstokhi@yahoo.com>
2023-06-13 23:20:53 -0700

    Add checkWinner, checkTie functions
    Added functions that check for game completion.
    Still working on completing checkWinner function, but
    converted board to an Object instead of an array to
    include index key-value pair to values at winning index.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const Gameboard = (() => {
     const board = [];
-
+    let k = 1;
+    
     for (let i = 0; i < 3; i++) {
         board[i] = [];
         for (let j = 0; j < 3; j++){
-            board[i].push(null)
+            board[i].push({token: null, index: k});
+            k++;
         }
     }
 
     const getBoard = () => board;
 
     const addToken = (row, column, player) => {
-        board[row][column] = player.getToken()
+        board[row][column].token = player.getToken()
     }
 
     return {getBoard, addToken}
@@ -31,21 +33,24 @@ const GameController = ((playerOne = 'Player 1', playerTwo = 'Player 2') => {
     const players = [Player(playerOne, 1), Player(playerTwo, 2)];
 
     let selectedPlayer = players[0];
+    let moveCount = 0;
 
     const playRound = () => {
         console.log(`Your turn ${selectedPlayer.getPlayer()}`)
         const row = parseInt(prompt('Select Row:'))
         const column = parseInt(prompt('Select Column'))
 
-        if (board.getBoard()[row][column] != null) {
+        if (board.getBoard()[row][column].token != null) {
             console.log('Invalid Move. Try Again');
         } else {
             board.addToken(row, column, selectedPlayer);
+            moveCount += 1;
             switchPlayers();
             console.log(`You're next ${selectedPlayer.getPlayer()}`);
         }
         
-        
+        if (moveCount >= 6) {checkWinner()}
+
         return board.getBoard()
     }
 
@@ -55,8 +60,17 @@ const GameController = ((playerOne = 'Player 1', playerTwo = 'Player 2') => {
         } else {selectedPlayer = players[0]}
     }
 
-    const checkBoard = () => {
+    const checkWinner = () => {
         
+    }
+
+    const checkTie = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === null) return
+            }
+        }
+        console.log('Tie')
     }
 
     return {playRound}
